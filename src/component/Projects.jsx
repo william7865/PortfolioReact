1bb54b0f866ef3d0d7f285182c6c460eb7b3a2c6
William Lin <linwilliam14@gmail.com>
2024-11-06 11:57:22 +0100

    Project end

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "../styles/Projects.css";
+import garage from "../assets/garage.png";
+import resto from "../assets/resto.png";
+import voyage from "../assets/voyage.png";
+const Projects = ({ translations }) => {
+    const projectListRef = useRef(null);
 
-// Ajoutez un div qui permet de slide vers la gauche ou la droite pour voir les projets suivants ou précédents.
-const Projects = ({translations}) => {
+    const scrollLeft = () => {
+        const projectWidth = projectListRef.current.firstChild.offsetWidth;
+        projectListRef.current.scrollBy({ left: -projectWidth, behavior: 'smooth' });
+    };
+
+    const scrollRight = () => {
+        const projectWidth = projectListRef.current.firstChild.offsetWidth;
+        projectListRef.current.scrollBy({ left: projectWidth, behavior: 'smooth' });
+    };
+
     return (
         <section id="projects" className="projects">
-        <h3>{translations.projects}</h3>
-        <div className="project-list">
-          <div className="project">
-            <h4>Projet 1</h4>
-            <p>Description du projet 1.</p>
-          </div>
-          <div className="project">
-            <h4>Projet 2</h4>
-            <p>Description du projet 2.</p>
-          </div>
-          <div className="project">
-            <h4>Projet 3</h4>
-            <p>Description du projet 3.</p>
-          </div>
-        </div>
-      </section>
+            <h3>{translations.projects}</h3>
+            <div className="project-container">
+                <button className="scroll-button left" onClick={scrollLeft}>
+                    ◀
+                </button>
+                <div className="project-list" ref={projectListRef}>
+                    <div className="project">
+                        <img src={garage} alt="Projet 1" className="project-image" />
+                        <h4>Projet 1</h4>
+                        <p>Description du projet 1.</p>
+                    </div>
+                    <div className="project">
+                        <img src= {resto}alt="Projet 2" className="project-image" />
+                        <h4>Projet 2</h4>
+                        <p>Description du projet 2.</p>
+                    </div>
+                    <div className="project">
+                        <img src={voyage}alt="Projet 3" className="project-image" />
+                        <h4>Projet 3</h4>
+                        <p>Description du projet 3.</p>
+                    </div>
+                </div>
+                <button className="scroll-button right" onClick={scrollRight}>
+                    ▶
+                </button>
+            </div>
+        </section>
     );
-}
+};
+
 export default Projects;
